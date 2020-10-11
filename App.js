efc073f07711b6aa2e8630a8ee1ae12c2134f169
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-11 10:35:08 +0200

    add FlatList

--- App.js
+++ App.js
@@ -1,52 +1,31 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 
 export default function App() {
 
-  const [people, setPeople] = useState([
-    { name: 'Eden', id: '1' },
-    { name: 'Kevin', id: '2' },
-    { name: 'Thbibault', id: '3' },
-    { name: 'Romelu', id: '4' },
-    { name: 'Karim', id: '5' },
-    { name: 'Lionel', id: '6' },
-    { name: 'Cristiano', id: '7' },
-  ]);
+  const [todos, setTodo] = useState([
+    { text: 'Mealprepping', key: '1' },
+    { text: 'Nutrition Advice', key: '2' },
+    { text: 'Groceries shopping', key: '3' },
 
-  const pressHandler = (id) => {
-    console.log(id);
-    setPeople((prevPeople) => {
-      return prevPeople.filter(person => person.id != id);
-    })
-  }
+  ]);
 
   return (
     <View style={styles.container}>
-
-    <FlatList
-      keyExtractor={(item) => item.id}
-      data={people}
-      renderItem={( { item } ) => (
-        <TouchableOpacity onPress={() => pressHandler(item.id)}>
-          <Text style={styles.item}>
-                {item.name}
-              </Text>
-        </TouchableOpacity>
-      )}
-    />
-
-      {/* <ScrollView>
-        { people.map(item => (
-            <View key={item.key}>
-              <Text style={styles.item}>
-                {item.name}
-              </Text>
-            </View>
-          ))}
-      </ScrollView> */}
-
-
+      {/* header */}
+      <View style={styles.content}>
+        {/* to form */}
+        <View style={styles.list}>
+          <FlatList 
+            data={todos}
+            renderItem={({ item }) =>  (
+              <Text>{item.text}</Text>
+            )}
+          />
+        </View>
+      </View>
     </View>
+
   );
 }
 
@@ -54,18 +33,13 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: 40,
-    paddingHorizontal: 20,
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
-  item: {
-    marginTop: 44,
-    padding: 30,
-    backgroundColor: 'pink',
-    fontSize: 24,
-    marginHorizontal: 10,
-    marginTop: 24,
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20,
   }
+  
 
 });
