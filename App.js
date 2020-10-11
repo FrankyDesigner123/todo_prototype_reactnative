fdd517318a11754b855cea27909d3a59242e09fe
Franky <62474988+ninja-uiux@users.noreply.github.com>
2020-10-11 08:42:11 +0200

    List & ScrollView

--- App.js
+++ App.js
@@ -1,32 +1,29 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
 
 export default function App() {
-  const [name, setName] = useState('Franky');
-  const [age, setAge] = useState(25);
+
+  const [people, setPeople] = useState([
+    { name: 'Eden', key: '1' },
+    { name: 'Kevin', key: '2' },
+    { name: 'Thbibault', key: '3' },
+    { name: 'Romelu', key: '4' },
+    { name: 'Karim', key: '5' },
+    { name: 'Lionel', key: '6' },
+    { name: 'Cristiano', key: '7' },
+  ]);
 
   return (
     <View style={styles.container}>
-      <Text>
-        Enter name:
-      </Text>
-      <TextInput 
-        multiline
-        style={styles.input}
-        placeholder='e.g. John Doe'
-        onChangeText={(val) => setName(val)} />
-
-      <Text>
-        Enter age:
-      </Text>
-      <TextInput 
-        keyboardType='numeric'
-        style={styles.input}
-        placeholder='e.g. 32'
-        onChangeText={(val) => setAge(val)} />
-
-
-      <Text>name: {name}, age {age}</Text>
+      <ScrollView>
+        { people.map(item => (
+            <View key={item.key}>
+              <Text style={styles.item}>
+                {item.name}
+              </Text>
+            </View>
+          ))}
+      </ScrollView>
     </View>
   );
 }
@@ -35,15 +32,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40,
+    paddingHorizontal: 20,
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
-
-  input: {
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
+  item: {
+    marginTop: 44,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   }
+
 });
